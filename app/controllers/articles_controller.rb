0109c6b60815b4378dc5640077f21bdf990ad9f3
lesliesage <leslie.sage@gmail.com>
2019-07-20 16:57:56 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,8 +25,8 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update(title: params[:article][:title], description: params[:description])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
-    # render :show  #this works the same as redirect here. 
+    # render :show  #this works the same as redirect here.
   end
 end
